de110aa4aef921b618126eb96e4eadab7c10abac
Gabriel <gabrielrojonovais@gmail.com>
2023-02-14 01:54:02 -0300

    memes from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import Nav from "./components/Nav"
 import Form from "./components/Form"
-import Img from "./components/Img"
-import { useState } from "react"
 
 function App() {
-  const [name, setName] = useState("Gabriel")
-  const [url, setUrl] = useState()
-  const getImg = (urlLink) => {
-    setUrl(urlLink)
-  }
-
+  
 return (
     <div>
       <Nav />
-      <Form getImg={getImg}/>
-      <Img url={url}/>
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,68 @@
 import styles from "./Form.module.css"
 import memes from "../memes"
+import { useState } from "react"
+import { useEffect } from "react"
 
 const Form = (props) => {
-    const getImg = props.getImg
+    const [memeAPI, setMemeAPI] = useState([])
+    const [meme, setMeme] = useState({
+        top: "",
+        bottom: "",
+        img: "",
+    })
+
+    useEffect(async () => {
+        const res = await fetch("https://api.imgflip.com/get_memes")
+        const data = await res.json()
+        setMemeAPI(data.data.memes)
+        //.then(res => {
+            //const data = res.json()
+            //return data
+        //})
+        //.then(data => setMemeAPI(data.data.memes))
+    },[])
+
+    const handleChange = (e) => {
+        setMeme(prev => {
+            return {
+                ...prev,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+    
     const handleClick = (e) => {
+        
         e.preventDefault()
         const num = Math.round(Math.random()*100)
-        const image = memes.data.memes[num].url
-        getImg(image)
+        const image = memeAPI[num].url
+        setMeme(prev => {
+            return {
+                ...prev,
+                img: image
+            }
+        })
+
     }
 
     return (
         <div className={styles.formcontainer}>
             <form>
                 <div className={styles.inputcontainer}>
-                    <input className={styles.input}type="text" placeholder="top text" />
-                    <input className={styles.input} type="text" placeholder="bottom text"/>
+                    <input value={meme.top} className={styles.input} type="text" placeholder="top text" name="top" onChange={(e) => handleChange(e)}/>
+                    <input value={meme.bottom} className={styles.input} type="text" placeholder="bottom text" name="bottom" onChange={handleChange}/>
                 </div>
                 <div>
                     <input onClick={handleClick} className={styles.btn} type="submit" value="Get a new meme image" />
                 </div>
             </form>
+            <div className={styles.imgcontainer}>
+                <div className={styles.frasecontainer}>
+                    <p className={styles.texttop}>{meme.top}</p>
+                    <p className={styles.textbottom}>{meme.bottom}</p>     
+                </div>
+                <img className={styles.img} src={meme.img} alt="" srcset="" />
+            </div>
         </div>
     )
 }
