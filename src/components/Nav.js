adf1b764e4104741ced7514c05c07577fd1d66ec
Gabriel <gabrielrojonovais@gmail.com>
2023-02-15 09:30:08 -0300

    finalizado

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
     return (
         <div className={styles.navcontainer}>
             <h3>Meme Generetor</h3>
-            <p>React</p>
+            <p>React!</p>
         </div>
     )
 }
